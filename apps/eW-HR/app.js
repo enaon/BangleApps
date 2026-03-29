b33535ef2f155cc7118d958ebc393a5cf02a74c0
enaon <enaon@drinet.gr>
2026-03-29 12:33:16 +0300

    up

--- apps/eW-HR/app.js
+++ apps/eW-HR/app.js
@@ -380,54 +380,7 @@ ew.face[0] = {
         let timeStr = "time: " + hours + ":" + mins;
         ew.UI.btn.c2l("main", "_main", 6, timeStr, "", 15, 1, 18, 2, "Vector");
     },
-    /*dG: function (update, newPos) {
-        if (ew.UI.ntid) return;
-
-        const margin = 2;
-        const width = g.getWidth() - margin;
-        const graphTop = 130;
-        const graphHeight = 45;
-        const fields = this.data.source.length;
-        const space = 3;
-        const topL = this.data.topL;
-        const btmL = this.data.btmL;
-
-        if (fields === 0) return;
-
-        const MAX_BAR_WIDTH = 30;
-
-        let bw, startX;
-        if (fields * (MAX_BAR_WIDTH + space) <= width) {
-            bw = MAX_BAR_WIDTH;
-            let totalWidth = fields * (bw + space);
-            startX = margin + (width - totalWidth) / 2;
-        }
-        else {
-            bw = (width - (fields * space)) / fields;
-            startX = margin;
-        }
-
-        let scale = graphHeight / this.data.max;
-
-        for (let i = 0; i < fields; i++) {
-            let entry = this.data.source[i];
-            let barH = entry.bpm * scale;
-            let x = startX + i * (bw + space);
-            let isSelected = (i === this.data.pos);
-
-            let color = topL < entry.bpm ? 13 : entry.bpm < btmL ? 9 : 4;
-
-            g.setCol(1, isSelected ? 15 : color);
-            g.fillRect(x, graphTop + graphHeight - barH, x + bw, graphTop + graphHeight);
-        }
-
-        if (this.tid) clearTimeout(this.tid);
-        this.tid = setTimeout(() => {
-            this.tid = 0;
-            this.dI();
-        }, 50);
-    },
-*/
+ 
    dG: function(update, newPos) {
         if (ew.UI.ntid) return;
 
