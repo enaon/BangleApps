51a2988fbe6670841be89ddfbada50e838e5a557
enaon <enaon@drinet.gr>
2026-03-31 10:26:36 +0300

    up

--- apps/eW-UI/UI.js
+++ apps/eW-UI/UI.js
@@ -573,6 +573,8 @@ ew.sys.TC = {
   },
   bar: function (data) {
     "ram";
+ 		if (this.nav) {this.nav=0; return;};
+
     // ---- finger on screen ----
     if (data.b) {
 
@@ -594,28 +596,27 @@ ew.sys.TC = {
       }
 
       // ---- slide mode ----
-      if (this.val.reverce)
-        this.tmp -= data.dx;
-      else
-        this.tmp += data.dx;
-      let lim = (200 / (this.val.up - this.val.dn)) | 0;
-      let len = this.val.len || lim < 10 ? 10 : 50 < lim ? 50 : lim;
-      this.step = (this.tmp / len) | 0;
-      if (this.step) {
-        this.slide = 1;
-        if (this.tid.long) {
-          clearTimeout(this.tid.long);
-          this.tid.long = 0;
-        }
-        let abs = (this.step < 0 ? -this.step : this.step);
-        this.val.cur += (abs < 3 ? this.step : (abs * this.step / 2) | 0);
-        this.tmp = 0;
-        if (this.val.up < this.val.cur) this.val.cur = (this.val.loop) ? this.val.dn : this.val.up;
-        else if (this.val.cur < this.val.dn) this.val.cur = (this.val.loop) ? this.val.up : this.val.dn;
-        ew.sys.buzz.nav(10);
-        ew.UI.c.tcBar(this.side, this.val.cur);
+
+			this.tmp += (this.val.reverce?-data.dx:data.dx);;
+			this.tmp += data.dx;
+			let lim = (300 / (this.val.up - this.val.dn)) | 0;
+			let len = this.val.len || lim < 20 ? 20 : 70 < lim ? 70 : lim;
+			this.step = (this.tmp / len) | 0;
+			if (this.step) {
+				this.slide = 1;
+				if (this.tid.long) {
+					clearTimeout(this.tid.long);
+					this.tid.long = 0;
+				}
+				let abs = (this.step < 0 ? -this.step : this.step);
+				this.val.cur += (abs < 3 ? this.step : (abs * this.step / 1.3) | 0);
+				this.tmp = 0;
+				if (this.val.up < this.val.cur) this.val.cur = (this.val.loop) ? this.val.dn : this.val.up;
+				else if (this.val.cur < this.val.dn) this.val.cur = (this.val.loop) ? this.val.up : this.val.dn;
+				ew.sys.buzz.nav(10);
+				ew.UI.c.tcBar(this.side, this.val.cur);
 			}
-      
+
       return;
     }
 
